e4abb34c1bf2b05241fab78039da1ecd27625f83
7test357 <189218812+7test357@users.noreply.github.com>
2024-12-17 22:07:28 +0900

    Create script.js

---
+++ ctf/sqli/script.js
@@ -0,0 +1,18 @@
+function checkLogin(event) {
+    event.preventDefault();
+
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username === "admin" && password === "admin") {
+        document.getElementById("message").style.display = "block";
+        document.getElementById("flag").style.display = "none";
+    } else if (username === "admin" && password.includes("' OR '1'='1")) {
+        document.getElementById("flag").style.display = "block";
+        document.getElementById("message").style.display = "none";
+    } else {
+        alert("Invalid credentials! Try again.");
+        document.getElementById("message").style.display = "none";
+        document.getElementById("flag").style.display = "none";
+    }
+}
