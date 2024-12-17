abfc45e061ece5779392fcc4f91e36f93a9507e2
7test357 <189218812+7test357@users.noreply.github.com>
2024-12-17 22:46:20 +0900

    Update script.js

--- ctf/sqli/script.js
+++ ctf/sqli/script.js
@@ -1,24 +1,22 @@
-(function () {
-    function checkLogin(event) {
-        event.preventDefault();
+(function() {
+    function _0x1d1f(_0x53f1) {
+        _0x53f1.preventDefault();
 
-        const username = document.getElementById("username").value;
-        const password = document.getElementById("password").value;
+        const _0x56a1 = document.getElementById("username").value;
+        const _0x2f72 = document.getElementById("password").value;
 
-        if (username === "admin" && password === "admin") {
+        if (_0x56a1 === "admin" && _0x2f72 === "admin") {
             document.getElementById("message").style.display = "block";
             document.getElementById("flag").style.display = "none";
-        } else if (
-            username.includes("admin' OR '1'='1")
-        ) {
+        } else if (_0x56a1.includes("admin' OR '1'='1")) {
             document.getElementById("flag").style.display = "block";
             document.getElementById("message").style.display = "none";
         } else {
-            alert("Invalid credentials! Try again.");
+            alert("Invalid\x20credentials!Try\x20again.");
             document.getElementById("message").style.display = "none";
             document.getElementById("flag").style.display = "none";
         }
     }
 
-    document.querySelector("form").addEventListener("submit", checkLogin);
+    document.querySelector("form").addEventListener("submit", _0x1d1f);
 })();
